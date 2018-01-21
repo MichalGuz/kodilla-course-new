1afd2fade307fc4ba6ffcd23335fdafe46f16e7d
MichalGuz <michalguz@gmail.com>
2018-01-21 23:04:11 +0100

    junit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,36 @@
 package com.kodilla;
 
 public class Calculator {
+    public int x;
+    public int y;
+
+    public Calculator(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int add(){
+        int add = x + y;
+        return add;
+    }
+    public int substract(){
+        int substract = x - y;
+        return substract;
+    }
+}
+
+class Operation{
+
+    public static void main (String[]args) throws java.lang.Exception{
+        Calculator operation1 = new Calculator(8,3);
+        System.out.println(operation1.substract());
+    }
 }
