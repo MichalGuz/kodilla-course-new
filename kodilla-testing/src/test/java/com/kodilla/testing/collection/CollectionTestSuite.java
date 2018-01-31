57038bf2b967cafc1b7ef9bf48dbb8e9877c5e00
MichalGuz <michalguz@gmail.com>
2018-01-31 17:01:32 +0100

    OddNumersExterminator

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,4 +1,57 @@
 package com.kodilla.testing.collection;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class CollectionTestSuite {
+
+    @Before
+    public void startTest(){
+        System.out.println("Start test");
+    }
+
+    @After
+    public void stopTest(){
+        System.out.println("Stop test");
+    }
+    @Test
+    public void testOddNumbersExterminatorEmptyList(){
+
+        //given
+        ArrayList<Integer> theList = new ArrayList<>();
+        OddNumbersExterminator on = new OddNumbersExterminator();
+
+        //when
+        ArrayList<Integer> result = on.exterminate(theList);
+
+        //then
+        Assert.assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testOddNumbersExterminatorNormalList(){
+
+        //given
+        ArrayList<Integer> theList = new ArrayList<>();
+        theList.add(3);
+        theList.add(2);
+        theList.add(11);
+        theList.add(25);
+        theList.add(5);
+        theList.add(6);
+        theList.add(13);
+        theList.add(31);
+        theList.add(32);
+        OddNumbersExterminator on = new OddNumbersExterminator();
+
+        //when
+        ArrayList<Integer> result = on.exterminate(theList);
+
+        //then
+        Assert.assertEquals(3, result.size());
+    }
 }
