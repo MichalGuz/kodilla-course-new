c972ec934e759c652c217b8c03204f088d9bda2e
MichalGuz <michalguz@gmail.com>
2018-02-21 20:45:24 +0100

    Stream.LocalDate.now()

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -14,12 +14,8 @@ public class StreamMain {
 
         Map<Integer, ForumUser> selection = forumSelect.getUserList().stream()
                 .filter(forumUser -> forumUser.getSexUser() == 'M')
-                //odfiltruje tylko tych użytkowników, którzy mają co najmniej 20 lat
                 .filter(forumUser -> forumUser.getBirthDate().isBefore(overTwenty))
-                // odfiltruje tylko tych użytkowników, którzy mają co najmniej jeden opublikowany post
                 .filter(forumUser -> forumUser.getPostsNumber() > 0)
-                //przy pomocy kolektora utworzy mapę par, w której rolę klucza będzie pełnił unikalny identyfikator użytkownika
-                //wyświetli otrzymaną mapę wynikową
                 .collect(Collectors.toMap(ForumUser::getForumUserID,ForumUser -> ForumUser));
 
         System.out.println("Numbers elements of list \"Selection\": " + selection.size());
