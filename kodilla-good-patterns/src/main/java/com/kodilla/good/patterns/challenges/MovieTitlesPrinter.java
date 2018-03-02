67b286c6a3b97bb03cc45a5817bb8787211ecf00
MichalGuz <michalguz@gmail.com>
2018-03-02 21:14:51 +0100

    collector joining

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieTitlesPrinter.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieTitlesPrinter.java
@@ -1,11 +1,14 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.util.stream.Collectors;
+
 public class MovieTitlesPrinter {
     public static void main(String[] args) {
         MovieStore movieTitles = new MovieStore();
-        movieTitles.getMovies().values().stream()
+        String result = movieTitles.getMovies().values().stream()
                 .flatMap(m -> m.stream())
-                .map(m -> m + " !")
-                .forEach(System.out::println);
+                .collect(Collectors.joining("! ","\n", "\n"  ));
+
+        System.out.println(result);
     }
 }
